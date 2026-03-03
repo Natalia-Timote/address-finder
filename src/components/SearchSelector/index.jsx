6f1448ef74618479dceead0c393220f40a2a1e47
Natalia-Timote <nataliamirian.nt@gmail.com>
2026-03-02 21:58:38 -0300

    Adição: imagem

--- src/components/SearchSelector/index.jsx
+++ src/components/SearchSelector/index.jsx
@@ -3,6 +3,7 @@ import './SearchSelector.style.css';
 export default function SearchSelector({ onChangeScreen }) {
     return (
         <section className='search-selector'>
+            <img className='adress-logo' src="./adress.png" alt="" />
             <h1>Adress Finder</h1>
             <button onClick={() => onChangeScreen('cep')}>Buscar com CEP</button>
             <button onClick={() => onChangeScreen('logradouro')}>Buscar com logradouro</button>
